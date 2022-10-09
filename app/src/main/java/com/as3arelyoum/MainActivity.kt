0a12a6fc050b48317c88e9979365aa26bb371d84
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-10-10 00:07:56 +0200

    Changed Local App to Arabic

--- app/src/main/java/com/as3arelyoum/MainActivity.kt
+++ app/src/main/java/com/as3arelyoum/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.as3arelyoum
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -7,13 +8,15 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.recyclerview.widget.GridLayoutManager
 import com.as3arelyoum.databinding.ActivityMainBinding
 import com.hugocastelani.waterfalltoolbar.Dp
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
     private var items: ArrayList<ItemsModel> = ArrayList()
-    private val viewModel : SplashScreenViewModel by viewModels()
+    private val viewModel: SplashScreenViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().apply {
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
                 viewModel.isLoading.value
             }
         }
+        setAppLocale("ar")
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -48,9 +52,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun Context.setAppLocale(language: String): Context {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val config = resources.configuration
+        config.setLocale(locale)
+        config.setLayoutDirection(locale)
+        return createConfigurationContext(config)
+    }
+
     private fun fakeData() {
         for (i in 1..20) {
-            items.add(ItemsModel("Amazon", R.mipmap.ic_launcher_round))
+            items.add(ItemsModel("أمازون", R.mipmap.light_logo))
         }
     }
 
