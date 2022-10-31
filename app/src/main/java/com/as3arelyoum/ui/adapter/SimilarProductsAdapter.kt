555206310c69d6446d45d8f784a97b640c71e16e
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-10-31 15:38:29 +0200

    Designed product details activity

---
+++ app/src/main/java/com/as3arelyoum/ui/adapter/SimilarProductsAdapter.kt
@@ -0,0 +1,54 @@
+package com.as3arelyoum.ui.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.as3arelyoum.data.model.Product
+import com.as3arelyoum.databinding.ProductCardBinding
+import com.bumptech.glide.Glide
+
+class SimilarProductsAdapter(
+    private var list: List<Product>,
+    private val onItemClicked: (position: Int) -> Unit
+) : RecyclerView.Adapter<SimilarProductsAdapter.CustomViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val recyclerCard =
+            ProductCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CustomViewHolder(recyclerCard, onItemClicked)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val productItems = list[position]
+        holder.binding.apply {
+            Glide.with(holder.binding.root.context)
+                .load(productItems.image_url)
+                .into(productImage)
+            nameTv.text = productItems.name
+            priceTv.text =  productItems.price + " " + "جنيه مصري"
+            sourceTv.text = "من" + " " + productItems.source
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class CustomViewHolder(
+        val binding: ProductCardBinding,
+        private val onItemClicked: (position: Int) -> Unit
+    ) :
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+
+        init {
+            binding.root.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            onItemClicked(absoluteAdapterPosition)
+        }
+    }
+}
