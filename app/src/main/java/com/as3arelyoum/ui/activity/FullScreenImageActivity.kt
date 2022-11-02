953bfea2e15b1bc3aec4a2fa61cc7826e9ef9447
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-11-02 17:36:08 +0200

    User Can show photo in full screen and download it

---
+++ app/src/main/java/com/as3arelyoum/ui/activity/FullScreenImageActivity.kt
@@ -0,0 +1,11 @@
+package com.as3arelyoum.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullScreenImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_image)
+    }
+}
