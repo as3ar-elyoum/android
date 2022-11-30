a5eb9fa7ef2c09f86c62a009b32accc318e46fe4
MoatazBadawy <moataz.egy101@gmail.com>
2022-11-30 03:42:59 +0200

    change from api that in viewmodel to repository "Your Viewmodel shouldn't know any thing about your api class or remote package"

--- app/src/main/java/com/as3arelyoum/data/repository/AssarRepository.kt
+++ app/src/main/java/com/as3arelyoum/data/repository/AssarRepository.kt
@@ -2,6 +2,7 @@ package com.as3arelyoum.data.repository
 
 import com.as3arelyoum.data.remote.dto.UserInfoDTO
 import com.as3arelyoum.data.remote.network.RetrofitInstance
+import com.google.gson.JsonObject
 
 class AssarRepository {
     private val assarApi = RetrofitInstance.assarApiService
@@ -9,6 +10,11 @@ class AssarRepository {
     suspend fun getAllCategories() = assarApi.getAllCategories()
 
     suspend fun getAllProducts(category_id: Int) = assarApi.getAllProducts(category_id)
+
+    suspend fun getProductDetails(product_id: Int) = assarApi.getProductDetails(product_id)
+
+    suspend fun updateProductDetails(product_id: Int, params: JsonObject) =
+        assarApi.updateProductDetails(product_id, params)
 
     suspend fun search(query: String) = assarApi.search(query)
 
--- app/src/main/java/com/as3arelyoum/ui/product/viewmodel/ProductDetailsViewModel.kt
+++ app/src/main/java/com/as3arelyoum/ui/product/viewmodel/ProductDetailsViewModel.kt
@@ -3,17 +3,17 @@ package com.as3arelyoum.ui.product.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
-import com.as3arelyoum.data.remote.network.RetrofitInstance
+import com.as3arelyoum.data.repository.AssarRepository
 import com.as3arelyoum.utils.status.Resource
 import com.google.gson.JsonObject
 
 class ProductDetailsViewModel : ViewModel() {
-    private val api = RetrofitInstance.assarApiService
+    private val repository = AssarRepository()
 
     fun getProductDetails(product_id: Int) = liveData {
         emit(Resource.loading(null))
         try {
-            emit(Resource.success(api.getProductDetails(product_id)))
+            emit(Resource.success(repository.getProductDetails(product_id)))
         } catch (e: Exception) {
             emit(Resource.error(null, e.message.toString()))
         }
@@ -24,7 +24,7 @@ class ProductDetailsViewModel : ViewModel() {
         try {
             emit(
                 Resource.success(
-                    api.updateProductDetails(
+                    repository.updateProductDetails(
                         product_id,
                         params
                     )
